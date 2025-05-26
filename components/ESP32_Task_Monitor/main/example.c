9d3788e972dd7eb14539ff987caf280a196c8f5f
Yukemi <42518199+Yukemi@users.noreply.github.com>
2025-05-26 23:43:31 +0200

    Delete components/ESP32_Task_Monitor directory

--- components/ESP32_Task_Monitor/main/example.c
+++
@@ -1,8 +0,0 @@
-#include "task_monitor.h"
-
-void app_main(void)
-{
-    // NOTE: configUSE_TRACE_FACILITY and configGENERATE_RUN_TIME_STATS must be defined as 1
-    // in FreeRTOSConfig.h for this API function task_monitor() to be available.
-    task_monitor();
-}
